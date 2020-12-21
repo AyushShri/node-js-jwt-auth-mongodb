e28b85133169dcda82cecf279b97a47d9dfb65ca
Ayush Shrivastav <shrivastavayush32@gmail.com>
2020-12-22 00:37:17 +0530

    Added server and Db Config

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports={
+    secret:"ayush-secret-key"
+};
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const bodyparser = require("body-parser")
+const cors = require("cors")
+const app = express()
+var corsOptions ={
+    origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server running Normally, Health : Good, Status: Active"})
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT,()=> {
+    console.log(`Server is running on port ${PORT}.`);
+});
